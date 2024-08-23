b1efce3948bdd5f4e6d606484042a55f8175da25
Royal-Aeo <aeo_royals@outlook.com>
2024-08-23 13:28:37 +0530

    dt 23/8/24

--- Rough/prtest.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int linearsearch(int *array){
-    for 
-}
-int main(){
-    int i[] = {1,2,3,4,5,6,7,8};
-}
--- Rough/rough.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-    int i;
-    for(i=0;i<900;i++){
-        printf("%c\n",i);
-    }
-}
