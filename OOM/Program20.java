6ad90f9306e30951846db62183ad9ca821dab870
Royal-Aeo <aeo_royals@outlook.com>
2024-09-13 13:21:58 +0530

    .

--- OOM/Program20.java
+++ OOM/Program20.java
@@ -1,20 +1,13 @@
 // WAP to compare two string using 
-import java.util.Scanner;
-
 public class Program20 {
     public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Input first String: ");
-        String s1 = scan.nextLine();
-        System.out.print("Input second String: ");
-        String s2 = scan.nextLine();
+    String s1 = "Hello";
+    String s2 = "hello";
         boolean x = s1.equalsIgnoreCase(s2);
         if (x) {
             System.out.println("Both the strings are equal");
         } else {
             System.out.println("Both the strings are not equal");
         }
-        scan.close();
     }
 }
